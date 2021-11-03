60c8a3b9536fac93828fb725d6a47599c06b3e65
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-03 13:00:30 +0700

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+    // console.log(event.target.value); //event.target.value is value from textbox
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {name}</h1>
+      <input
+        // onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        // value={name}
+      />
+      <button onClick={handleChange}>Submit</button>
     </div>
   );
 }
